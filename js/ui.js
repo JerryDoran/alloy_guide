c71d20a23d3c95e54a1c29bfd85ec9d076480001
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-07-31 14:28:21 -0400

    Initial commit

---
+++ js/ui.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Setup Materialize Components
+  const sideNav = document.querySelector('.side-menu');
+  M.Sidenav.init(sideNav, { edge: 'right' });
+});
